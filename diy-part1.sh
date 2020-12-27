af7b56c9c070b7496aa16131e9b9ac7e3417f6b4
liwenjie119 <liwenjie119@126.com>
2020-12-27 23:05:06 +0800

    upload x64

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/myapp/luci-app-adguardhome
+git clone https://github.com/2512500960/zzu-minieap-openwrt package/myapp/zzu-minieap-openwrt
+git clone https://github.com/tty228/luci-app-serverchan package/myapp/luci-app-serverchan
+git clone https://github.com/liwenjie119/luci-app-koolproxyR package/myapp/luci-app-koolproxyR
